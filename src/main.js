58ed2d347d08ac642cf345d496fdeecca834af23
Bertil-Bjoernboel <bert5659@stud.kea.dkgit config --global user.email Bert5659@stud.kea.dk>
2022-09-12 09:50:03 +0200

    comiitiy

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
 scroll(animate(".progress-bar", { scaleX: [0, 1] }));
+
+animate(
+  ".staggeranimation li",
+  { opacity: [0, 1] },
+  {
+    delay: stagger(1, { start: 2 }),
+  }
+);
