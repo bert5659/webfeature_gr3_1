3f866e8505f99d9a2c7d8d3d5576e7547e8d06d5
Bertil-Bjoernboel <bert5659@stud.kea.dkgit config --global user.email Bert5659@stud.kea.dk>
2022-09-12 09:29:31 +0200

    Styling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+scroll(animate(".progress-bar", { scaleX: [0, 1] }));
