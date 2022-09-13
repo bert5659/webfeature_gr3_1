ebc6b715cf0f550ca68937d0a35b117fdb56f98b
Bertil-Bjoernboel <bert5659@stud.kea.dkgit config --global user.email Bert5659@stud.kea.dk>
2022-09-13 10:45:12 +0200

    Javascript virker hallo

--- src/main.js
+++ src/main.js
@@ -12,22 +12,46 @@ animate(
   }
 );
 
+// document.querySelector("#glitch").addEventListener("click", billede);
+
+// function billede() {
+//   document.querySelector("#glitch").classList.add("opacity");
+// }
+
+// document.querySelector("#glitch2").addEventListener("click", billede2);
+
+// function billede2() {
+//   document.querySelector("#glitch2").classList.add("opacity");
+// }
+
+// document.querySelector("#glitch2").addEventListener("animationend", visBillede);
+
+// function visBillede() {
+//   document.querySelector("#glitch").classList.add("opacityfull");
+// }
+
+// billede 1 glitchart
 document.querySelector("#glitch").addEventListener("click", billede);
 
 function billede() {
   document.querySelector("#glitch").classList.add("opacity");
 }
 
-document.querySelector("#glitch2").addEventListener("click", billede2);
+document.querySelector("#glitch").addEventListener("animationend", visBillede2);
 
-function billede2() {
-  document.querySelector("#glitch2").classList.add("opacity");
+function visBillede2() {
+  document.querySelector("#glitch").classList.add("opacityfull");
 }
 
-document.querySelector("#glitch2").addEventListener("animationend", visBillede);
-document.querySelector("#glitch").addEventListener("animationend", visBillede);
+// billede analog
+document.querySelector("#analog").addEventListener("click", analogEt);
 
-function visBillede() {
-  document.querySelector("#glitch").classList.add("opacityfull");
-  document.querySelector("#glitch2").classList.add("opacityfull");
+function analogEt() {
+  document.querySelector("#analog").classList.add("opacity");
+}
+
+document.querySelector("#analog").addEventListener("animationend", visAnalog);
+
+function visAnalog() {
+  document.querySelector("#analog").classList.add("opacityfull");
 }
